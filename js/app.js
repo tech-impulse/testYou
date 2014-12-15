6be3ece3eb2383748facaeadcfb28ac5c03b2dc2
Alain <alain.cidrera@tech-impulse.com>
2014-12-15 09:06:37 +0100

    test 2

--- js/app.js
+++ js/app.js
@@ -425,7 +425,6 @@ $(document).on('pageinit', '#loginModule', function () {
 
     //Crear anuncio 2 - Botón de seleccionar por cerca de mi
     $('#innuevoAnuncio2Cerca').unbind('click').bind('click', function () {
-        alert("click 1");
         $("#divnuevoAnuncio2").hide();
         $("#inputnuevoAnuncio2").hide();
 
@@ -433,7 +432,6 @@ $(document).on('pageinit', '#loginModule', function () {
 
     //Crear anuncio 2 - Botón de seleccionar por Zona 
     $('#innuevoAnuncio2Zona').unbind('click').bind('click', function () {
-        alert("click 2");
         $("#divnuevoAnuncio2").show();
         $("#inputnuevoAnuncio2").hide();
         $("#inputnuevoAnuncio2").val("");
@@ -442,7 +440,6 @@ $(document).on('pageinit', '#loginModule', function () {
 
     //Crear anuncio 2 - Botón de seleccionar por Numero de pantalla
     $('#innuevoAnuncio2Numero').unbind('click').bind('click', function () {
-        alert("click 3");
         $("#divnuevoAnuncio2").hide();
         $("#inputnuevoAnuncio2").show();
         $("#inputnuevoAnuncio2").val("");
